875015b6c7c274d98d36a5b70e993aca60e130c9
MaxMishchenko <dev.max.frontend.js@gmail.com>
2019-11-23 14:29:12 +0200

    Reworked

--- homework_6.js
+++ homework_6.js
@@ -1,39 +1,47 @@
-/*Find in array: sum, min and max, replace min and max - create custom functions*/
-console.log('-- Task 1 --')
+"use strict";
 
-let array = [30, 21, 7, 42, 70];
+console.log('-- Task 1 --');
+console.log("Find in array: sum, min and max, replace min and max - create custom functions");
 
-console.log('We have array: 30, 21, 7, 42, 70')
+let array = [10, -19, 128, -3, 15, 70, 67, 30, 21]; // Our Array
 
-let min = Math.min.apply(null, array);
+console.log("We have array: [10,-19,128,-3,15,70,67,30,21]");
 
-console.log("Min number of this array = " + min);
+let min = Math.min.apply(null, array); // Find the minimum number of array
 
-let max = Math.max.apply(null, array);
+console.log("In this array min = " + min);
 
-console.log("Max number of this array = " + max);
+let max = Math.max.apply(null, array); // Find the maximum number of array
 
-function arraySum(array) {
+console.log("In this array max = " + max);
+
+function arraySum(array) { // Find the sum of all the numbers in the array
     let sum = 0;
-    for (var i = 0; i < array.length; i++) {
-        sum += array[i];
+    for (let s = 0; s < array.length; s++) {
+
+        sum = sum + array[s];
     }
     console.log("Sum of array numbers = " + sum);
-}
+};
+
 arraySum(array);
 
-function ReplaceMinMax(array) {
-    let ReplaceMin = array[array.indexOf(Math.max.apply(null, array), 0)];
-    array[array.indexOf(Math.max.apply(null, array), 0)] = array[array.indexOf(Math.min.apply(null, array), 0)];
-    array[array.indexOf(Math.min.apply(null, array), 0)] = ReplaceMin;
-    console.log("Min Number After Replace  = " + ReplaceMin);
-}
-ReplaceMinMax(array);
+let position = array.indexOf(max); // Replace min and max in array
+
+if (position < max) {
+    array[position] = min;
+};
+position = array.indexOf(min);
+
+if (position > min) {
+    array[position] = max;
+};
+
+console.log("After replace min and max we have next array: " + "[" + array + "]");
 
 /*Create function which will return function with callback in arguments*/
-console.log('-- Task 2 --')
-
-console.log('See alert in top of you screen')
+console.log('-- Task 2 --');
+console.log('See alert in top of you screen');
 
 function LazyMan(sleep, callback) {
     alert(`Are you ${sleep} bro ?`);
@@ -45,34 +53,32 @@ LazyMan('sleeping', function() {
 });
 
 /*Create a function that will replace all number dividing three with ‘foo’, dividing seven with ‘bar’ and dividing three and seven with ‘foobar’. Function with n params.*/
-console.log('-- Task 3 --')
-console.log('From our array: 30, 21, 7, 42, 70')
-console.log('Numbers that are divided by 3 = foo')
-console.log('Numbers that are divided by 7 = bar')
-console.log('Numbers that are divided by 3 and 7 = foobar')
+console.log('-- Task 3 --');
+console.log('From our array: 10,-19,128,-3,15,70,67,30,21');
+console.log('Numbers that are divided by 3 = foo');
+console.log('Numbers that are divided by 7 = bar');
+console.log('Numbers that are divided by 3 and 7 = foobar');
 
 function ReplaceNumbers(array) {
-    for (let i = 0; i < array.length; i++) {
+    for (let n = 0; n < array.length; n++) {
 
-        if (array[i] % 3 == 0 && array[i] % 7 == 0) {
-            array[i] = "foobar"
-        } else if (array[i] % 7 == 0) {
-            array[i] = "bar"
-        } else if (array[i] % 3 == 0) {
-            array[i] = "foo"
+        if (array[n] % 3 === 0 && array[n] % 7 === 0) {
+            array[n] = "foobar"
+        } else if (array[n] % 7 === 0) {
+            array[n] = "bar"
+        } else if (array[n] % 3 === 0) {
+            array[n] = "foo"
         }
 
     }
 
-    console.log(array)
-}
-ReplaceNumbers(array)
-
+    console.log(array);
+};
+ReplaceNumbers(array);
 
 /*Create function with 2 string params. It must check if letters in the first correspond to the number of matches in second and return %.*/
-console.log('-- Task 4 --')
-
-console.log('We Have Two Words: GeekHub and GitHub')
+console.log('-- Task 4 --');
+console.log('We Have Two Words: GeekHub and GitHub');
 
 function LetterCheck(a, b) {
     let coincidence = 0
@@ -84,6 +90,6 @@ function LetterCheck(a, b) {
         }
     }
     let p = coincidence / (a.length + b.length) * 100;
-    console.log("Letter matches in this words = " + coincidence + " this in % = " + p)
-}
-LetterCheck("GeekHub", "GitHub")
+    console.log("Letter matches in this words = " + coincidence + " this in % = " + p);
+};
+LetterCheck("GeekHub", "GitHub");
